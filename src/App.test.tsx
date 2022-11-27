026b2a4583d6901eccdc6a507f35048a371c2f8a
Juan Alcantara <juan@cordage.io>
2022-11-26 19:14:10 -0600

    setting up router

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import Routes from './Routes';
 
 describe('App', () => {
   it('renders correctly', () => {
@@ -14,5 +16,17 @@ describe('App', () => {
     const textElement = screen.getByRole('heading');
 
     expect(textElement).toBeInTheDocument();
+  });
+
+  it('renders not found if invalid path', () => {
+    render(
+      <MemoryRouter initialEntries={['/badroute']}>
+        <Routes />
+      </MemoryRouter>
+    );
+
+    const notFoundText = screen.getByRole('heading');
+
+    expect(notFoundText).toHaveTextContent('404');
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { HashRouter } from 'react-router-dom';
+import Routes from './Routes';
 
 function App() {
   return (
     <HashRouter>
-      <h1>Hello World</h1>
+      <Routes />
     </HashRouter>
   );
 }
